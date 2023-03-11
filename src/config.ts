77b960cb1f9d5120aaf7c8f1be2b4595225c7d7c
来铧敏 <laihuamin@192.168.0.11>
2023-03-11 22:50:35 +0800

    feat: 提价nn

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { IConfig } from "./interface";
 dotenv.config();
 
 export const config: IConfig = {
-  openai_api_key: process.env.OPENAI_API_KEY || "123456789",
+  openai_api_key: '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' || "123456789",
   model: process.env.MODEL || "gpt-3.5-turbo",
   chatPrivateTiggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
   chatTiggerRule: process.env.CHAT_TRIGGER_RULE || "",
